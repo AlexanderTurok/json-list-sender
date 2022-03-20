278593f0c1255f489c7e3b7fcbe96e5b43563cf2
AlexanderTurok <alexander.turok2004@gmail.com>
2022-03-20 22:05:02 +0200

    simplify app

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 
+// install axios and import
 const axios = require("axios");
-const http  = require("http");
 
-// add json to src folder
-// and import like this
+// setup
 const data  = require("./data.json");
+const API = ""; // place your link to api
 
-const API = "http://localhost:8080/beat"; // place your link to api
-const hostname = "localhost"; // place your ip 
-const port = 5000; // place here your port
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Server Created Successfully...');
-});
-
+// --------------------------------------
 const parse = (data) => {
   data.map((item) => {
     axios.post(API, 
@@ -24,11 +15,5 @@ const parse = (data) => {
   })
 }
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-  parse(data);
-});
-
-
-
-
+// to start app simply type node index.js
+parse(data);
