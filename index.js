cf0cd02b29a3b98d8bbb1e9cf6960ea080f9208e
AlexanderTurok <alexander.turok2004@gmail.com>
2022-05-02 19:09:35 -0500

    fix

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const axios = require("axios");
 
 // setup
 const data  = require("./data.json");
-const API = ""; // place your link to api
+const API = "hhtp://localhost:8080/beat"; // place your link to api
 
 // --------------------------------------
 const parse = async (data) => {
-  await data.map((item) => {
-    axios.post(API, 
+  data.map((item) => {
+    await axios.post(API, 
       item
     )
   })
